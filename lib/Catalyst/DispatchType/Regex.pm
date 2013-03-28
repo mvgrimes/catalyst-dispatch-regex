736b2c8f3cfaf5d67460b87ad27d9089bdcfc598
Mark Grimes <mgrimes@cpan.org>
2013-03-28 11:52:34 -0400

    Changes the version number and adds note about acceptable version format
    - We need to be able to compare to a version Catalyst::Dispatcher via
      'le' (to keep things simple).

--- lib/Catalyst/DispatchType/Regex.pm
+++ lib/Catalyst/DispatchType/Regex.pm
@@ -21,7 +21,9 @@ has _attr => (
              );
 no Moose;
 
-our $VERSION = '5.90021';
+# Version needs to be in a format such that $VERSION gt '5.90020' => true
+# We use in Catalyst::Dispatcher ($VERSION le '5.90020')
+our $VERSION = '5.90020_1';
 
 =head1 NAME
 
